93f7ad8fe862e599a602d584846ddd99e5855a79
edewindt <edewindt27@outlook.com>
2022-12-20 12:45:58 -0600

    added conditional rendering

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class TestAppState extends State<TestApp> {
   ];
   int _i = 0;
   void answer(int n) {
-    if (_i < questions.length - 1 && n == 1) {
+    if (n == 1) {
       setState(() {
         _i = _i + n;
       });
@@ -65,21 +65,25 @@ class TestAppState extends State<TestApp> {
           backgroundColor: Colors.black,
           title: const Text('Test App'),
         ),
-        body: Column(children: [
-          Question(questions[_i]['question'] as String),
-          Column(
-            children: [
-              for (var i = 0; i < questions[_i]['answers'].length; i++)
-                Answer(i + 1, questions[_i]['answers'][i], change)
-            ],
-          ),
-          Nav(handle: answer),
-          if (chosen[_i] != '0')
-            Text(
-              chosen[_i],
-              style: TextStyle(fontSize: 20),
-            )
-        ]),
+        body: _i < questions.length
+            ? Column(children: [
+                Question(questions[_i]['question'] as String),
+                Column(
+                  children: [
+                    for (var i = 0; i < questions[_i]['answers'].length; i++)
+                      Answer(i + 1, questions[_i]['answers'][i], change)
+                  ],
+                ),
+                Nav(handle: answer),
+                if (chosen[_i] != '0')
+                  Text(
+                    chosen[_i],
+                    style: const TextStyle(fontSize: 20),
+                  )
+              ])
+            : Center(
+                child: Text('The End'),
+              ),
       ),
     );
   }
