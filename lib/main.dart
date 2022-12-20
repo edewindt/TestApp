447ba90363eefa982835d2c46cd64feb283f615c
edewindt <edewindt27@outlook.com>
2022-12-20 10:38:44 -0600

    added questions map

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,29 @@ import 'package:flutter/material.dart';
 void main() => runApp(TestApp());
 
 class TestApp extends StatelessWidget {
+  final List<Map> questions = const [
+    {
+      'question': 'What is your age range?',
+      'answers': ['0 - 13', '13 - 18', '21 - 49', '50+']
+    },
+    {
+      'question': 'What is your income bracket?',
+      'answers': [
+        '0 - 30,000',
+        '30,000 - 70,000',
+        '70,000 - 150,000',
+        '150,000+'
+      ]
+    },
+    {
+      'question': 'Have you ever lied?',
+      'answers': ['Yes', 'No']
+    },
+    {
+      'question': 'Are these annoying questions?',
+      'answers': ['Yes', 'No']
+    }
+  ];
   const TestApp({super.key});
 
   @override
